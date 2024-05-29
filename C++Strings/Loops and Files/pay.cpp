801313812fbdbd32ca3a1a200b1ad23be6703ebc
khawajaacode <156354232+khawajaacode@users.noreply.github.com>
2024-05-30 01:12:34 +0500

    Create pay.cpp

---
+++ C++Strings/Loops and Files/pay.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int pennies = 0;
+    int numberofdays;
+
+    cout << "Enter the number of days: ";
+    cin >> numberofdays;
+
+    if (numberofdays < 0)
+    {
+        cout << "Invalid Input" << endl;
+        return 1; 
+    }
+
+    cout << "Day\tSalary\n";
+    cout << "------------------\n";
+
+    for (int i = 0; i < numberofdays; i++)
+    {
+        pennies = ++pennies;
+        cout << i + 1 << "\t$" << fixed << setprecision(2) << pennies << "\n";
+    }
+
+    int Total = pennies; // Removed the unnecessary '+'
+    cout << "------------------\n";
+    cout << "Total Pay: $" << fixed << setprecision(2) << Total << "\n";
+
+    return 0;
+}
